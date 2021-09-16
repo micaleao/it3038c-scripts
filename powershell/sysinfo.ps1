658ecb9259f2b95837380ad759a39b42128b9f87
micaleao <micaleao@mail.uc.edu>
2021-09-16 17:35:37 -0400

    lab3

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,6 +1,16 @@
 ﻿function getIP {
     (Get-NetIPAddress).IPv4Address | Select-String "192*"
 }
+function PSV {
+    (Get-Host).Version | Select-String "5*"
+}
+
 $IP = getIP
-write-host("This machine's IP is $IP")
-write-host("This machine's IP is {0}" -f $IP)
+$USER = $env:USERNAME
+$Ht = $env:COMPUTERNAME
+$PSV = PSV
+$Date = Get-Date
+
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $Ht. Powershell version $PSV. Today's date is $Date." 
+
+Send-MailMessage -To "botheaj@ucmail.uc.edu" -From "amicale812@gmail.com" -Subject "IT3038c" -Body $BODY -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
