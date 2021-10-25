cf26dbf4df1157b7d7fe6dd894bf743028b86b1c
micaleao <micaleao@mail.uc.edu>
2021-10-25 13:17:51 -0400

    commit

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -13,4 +13,4 @@ $Date = Get-Date
 
 $BODY = "This machine's IP is $IP. User is $USER. Hostname is $Ht. Powershell version $PSV. Today's date is $Date." 
 
-Send-MailMessage -To "botheaj@ucmail.uc.edu" -From "amicale812@gmail.com" -Subject "IT3038c" -Body $BODY -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
+#Send-MailMessage -To "botheaj@ucmail.uc.edu" -From "amicale812@gmail.com" -Subject "IT3038c" -Body $BODY -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
