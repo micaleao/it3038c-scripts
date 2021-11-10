6b72cd8cb9c3280f991e67cbebf63fcba4a8923a
micaleao <micaleao@mail.uc.edu>
2021-11-10 15:19:37 -0500

    lab8

--- python/webscraperio.py
+++ python/webscraperio.py
@@ -1,8 +1,12 @@
 import requests, re 
 from bs4 import BeautifulSoup
 
-r = requests.get("http://webscraper.io/test-sites/e-commerce/allinone/phones").content
-soup = BeautifulSoup(r, "lmxl")
-tags = soup.findAll("a", {"href":re.compile('(phones)')})
-for a in tags:
-    print(a.get('href'))
+data = requests.get("https://www.microcenter.com/product/611893/logitech-mx-master-3-advanced-wireless-mouse-black").content
+soup = BeautifulSoup(data, 'html.parser')
+details = soup.find("h1")
+thisspan = ""
+for d in details:
+    title = d.find("span")
+if title is not None and title != -1:
+    thisspan = title
+print("The price of "+ thisspan.__str__('data-name'), "is "+ thisspan.__str__('data-price'))
